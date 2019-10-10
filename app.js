2f3aa8bbeb79212ee1f5a7a5151c3785bebe37f2
Neaxic <andreas79001@gmail.com>
2019-10-10 18:23:42 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+ app.get('/',function(req, res) {
+    res.sendFile(__dirname + '/client');
+});
+// app.use('/client',express.static(__dirname + '/client'));
+
+app.use(express.static('client'));
+
+serv.listen(8080);
+console.log('Server started');
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection',function(socket){
+    console.log(`New connection ${socket.id}`);
+
+    socket.on('PositionEvent', function(data){
+        console.log('Position: ' + 'x: ' + data.x, 'y: ' + data.y);
+        socket.broadcast.emit('NewPositionEvent',data);
+    });
+
+    // socket.emit('NewPositionEvent',{
+    //     data.x
+    // });
+});
+
+// // function newConnection(socket){
+// //     socket.on('positionEvent', (data) => {
+// //         // console.log(data);
+// //         socket.broadcast.emit('positionEvent',data);
+    
+// //         const pack = {
+// //           x: data.x,
+// //           y: data.y,
+// //         };
+
+// //         console.log(data);
+// //         socket.emit('positionEvent', pack);
+// //     });
+
+//       // function mouseMsg(data){
+//   //   socket.broadcast.emit('positionEvent',data);
+//   //   console.log(data),
---
+++ client/sketch.js
@@ -0,0 +1,129 @@
+
+// COLOR PICKER
+// var img
+// var c = 0;
+// function preload() {
+//   img = loadImage("gradient.png");
+// }
+
+var socket = io();
+
+var r = 255;
+var g = 255;
+var b = 255;
+var rainbow = 0;
+
+function ColorWhite() {
+  r = 255;
+  g = 255;
+  b = 255;
+  //document.getElementById("myBtnGreen").innerHTML = "YOU CLICKED ME!";
+}
+
+function ColorGreen() {
+  r = 152;
+  g = 251;
+  b = 152;
+  //document.getElementById("myBtnGreen").innerHTML = "YOU CLICKED ME!";
+}
+
+function ColorBlue() {
+  r = 128;
+  g = 206;
+  b = 255;
+  //document.getElementById("myBtnGreen").innerHTML = "YOU CLICKED ME!";
+}
+
+function ColorPurple() {
+  r = 194;
+  g = 157;
+  b = 253;
+  //document.getElementById("myBtnGreen").innerHTML = "YOU CLICKED ME!";
+}
+
+function ColorRed() {
+  r = 255;
+  g = 105;
+  b = 97;
+  //document.getElementById("myBtnGreen").innerHTML = "YOU CLICKED ME!";
+}
+
+function ColorYellow() {
+  r = 253;
+  g = 253;
+  b = 150;
+  //document.getElementById("myBtnGreen").innerHTML = "YOU CLICKED ME!";
+}
+
+function ColorEraser() {
+  r = 0;
+  g = 0;
+  b = 0;
+  //document.getElementById("myBtnGreen").innerHTML = "YOU CLICKED ME!";
+}
+
+function setup() {
+    var canvas = createCanvas(1900, 580);
+    canvas.parent('sketch-holder');
+    background(0, 0, 0);
+    // socket.on('mouse', newDrawing);
+    // socket.on('positionEvent',newPositionMsg);
+    //image(img, 0, 0, 400, 400);
+
+    //Button Reset
+      // button = createButton('Reset');
+      // button.position(45, 950);
+      // button.mousePressed(ResetButton);
+      
+      // function ResetButton(){
+      //   const colorData = {
+      //   f: 99
+      // }      
+        
+      // }
+  }
+
+  // function mouseClicked(){
+  //   var valp = 100;
+  //   const colorData = {
+  //     x: valp,
+  //   };
+  //   socket.emit("positionEvent",colorData);
+  //   console.log(colorData);
+  // }
+
+  // function newPositionMsg(posData) {
+  //   // console.log("got: ");
+  //   // console.log(posData);
+  //   positionRecived(posData);
+  // }
+
+  function mouseDragged() {
+    const x = mouseX;
+    const y = mouseY;
+    fill (r,g,b);
+    noStroke();
+    ellipse(x,y,20,20);
+    
+    let pos = {
+     x: x,
+     y: y,
+    };
+
+    console.log(pos)
+
+    socket.emit('PositionEvent',{
+        x: x,
+        y: y,
+        r: r,
+        g: g,
+        b: b,
+      });
+}
+
+socket.on('NewPositionEvent',positionRecived);
+
+  function positionRecived(data){
+    fill (data.r,data.g,data.b);
+    ellipse(data.x,data.y,20,20);
+  }
